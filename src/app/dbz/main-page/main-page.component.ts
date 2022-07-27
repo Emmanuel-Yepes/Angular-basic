f69ab106d49e30e76f30159a05db4eb32bb5f695
Emmanuel Yepes <emmanuelycdev@gmail.com>
2022-07-27 15:01:24 -0500

    Final de la sesion 5: 'Expandiendo nuestras bases' 27/07/2022

--- src/app/dbz/main-page/main-page.component.ts
+++ src/app/dbz/main-page/main-page.component.ts
@@ -1,9 +1,6 @@
 import { Component } from '@angular/core';
-
-interface Personaje {
-  nombre:string;
-  poder: number;
-}
+import { Personaje } from '../interfaces/dbz.interface';
+import { DbzService } from '../services/dbz.service';
 
 @Component({
   selector: 'app-main-page',
@@ -11,29 +8,12 @@ interface Personaje {
 })
 export class MainPageComponent {
 
-  personajes: Personaje[] = [
-    {
-      nombre: 'Goku',
-      poder:15000,
-    },
-    {
-      nombre: 'Vegeta',
-      poder:7500,
-    },
-  ];
-
   nuevo: Personaje = {
-    nombre: '',
-    poder: 0,
+    nombre: 'Maestro Roshi',
+    poder: 2000,
   }
 
-  agregar(){
-    if(this.nuevo.nombre.trim().length === 0){ return alert('asigna el nombre del personje'); }
-    this.personajes.push(this.nuevo);
-    this.nuevo = {
-      nombre: '',
-      poder: 0
-    };
-  }
+  constructor(){ 
 
+  }
 }
